4b7208362e1136f8b0fa056373e2b10b8b38999a
Jonah Kanner <jkanner@caltech.edu>
2020-07-27 15:56:11 -0700

    added real data feature

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def makesine(freq, amp):
     sig1 = TimeSeries(y1, dt=1.0/fs).taper()
     fig_sig1 = sig1.plot()
     plt.xlim(0,0.3)
+    plt.ylim(-5,5)
     st.pyplot(fig_sig1)
 
     return(sig1)
@@ -53,7 +54,90 @@ st.pyplot(figsum)
 
 st.markdown("## Convert to the frequency domain")
 
+plt.figure()
 freqdomain = signal.fft()
-st.pyplot(np.abs(freqdomain).plot())
+sigfig = np.abs(freqdomain).plot()
+plt.ylim(0,5)
+st.pyplot(sigfig)
 
 
+st.markdown("## Try a band-pass filter")
+
+
+lowfreq = st.slider("Low frequency cut-off", 1, 49, 20)
+highfreq = st.slider("High frequency cut-off", 50, 300, 100)
+bp_data = signal.bandpass(lowfreq, highfreq)
+
+plt.figure()
+st.pyplot(bp_data.plot())
+
+plt.figure()
+freqdomain = bp_data.fft()
+bpfig = np.abs(freqdomain).plot()
+plt.ylim(0,5)
+st.pyplot(bpfig)
+
+
+st.markdown("## Try Whitening")
+
+# white = signal.whiten()
+white = sig1 / amp1 + sig2 / amp2 + sig3/amp3
+st.pyplot(white.plot())
+
+plt.figure()
+freqdomain = white.fft()
+whitefig = np.abs(freqdomain).plot()
+plt.ylim(0,5)
+st.pyplot(whitefig)
+
+st.markdown("# Now try it with real data")
+
+# -- Close all open figures
+plt.close('all')
+
+@st.cache   #-- Magic command to cache data
+def load_gw(t0, detector):
+    strain = TimeSeries.fetch_open_data(detector, t0-18, t0+18, cache=False)
+    return strain
+
+
+detector = 'H1'
+t0 = 1126259462.4   #-- GW150914
+strain = load_gw(t0, detector)
+center = int(t0)
+strain = strain.crop(center-16, center+16)
+
+#-- Make a time series plot    
+
+st.subheader('Raw data')
+plt.figure()
+fig1 = strain.plot()
+plt.xlim(t0-0.2, t0+0.1)
+st.pyplot(fig1)
+
+# -- Plot psd
+plt.figure()
+psdfig = strain.psd().plot()
+plt.xlim(10, 1800)
+st.pyplot(psdfig)
+
+# -- Try whitened and band-passed plot
+# -- Whiten and bandpass data
+st.subheader('Whitened and Bandbassed Data')
+white_data = strain.whiten()
+bp_data = white_data.bandpass(30, 400)
+fig3 = bp_data.plot()
+plt.xlim(t0-0.2, t0+0.1)
+st.pyplot(fig3)
+
+
+# -- PSD of whitened data
+# -- Plot psd
+plt.figure()
+psdfig = bp_data.psd().plot()
+plt.xlim(10, 1800)
+st.pyplot(psdfig)
+
+
+# -- Close all open figures
+plt.close('all')
