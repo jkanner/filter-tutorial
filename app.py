58f374fa44024e86f81f53a04f825935d060c8a0
Jonah Kanner <jkanner@caltech.edu>
2020-07-27 14:16:11 -0700

    try to fix plot bug

--- app.py
+++ app.py
@@ -23,18 +23,22 @@ def makesine(freq, amp):
     y1 = amp*np.sin( 2*np.pi*freq*time )
     sig1 = TimeSeries(y1, dt=1.0/fs).taper()
     fig_sig1 = sig1.plot()
+    plt.xlim(0,0.3)
     st.pyplot(fig_sig1)
 
     return(sig1)
 
+plt.figure()
 freq1 = st.slider("Frequency", 20, 200, 20)
 amp1 = st.slider("Amplitude", 1.0, 10.0, 5.0)
 sig1 = makesine(freq1, amp1)
 
+plt.figure()
 freq2 = st.slider("Frequency", 20, 200, 47)
 amp2 = st.slider("Amplitude", 1.0, 10.0, 2.0)
 sig2 = makesine(freq2, amp2)
 
+plt.figure()
 freq3 = st.slider("Frequency", 20, 200, 195)
 amp3 = st.slider("Amplitude", 1.0, 10.0, 1.0)
 sig3 = makesine(freq3, amp3)
